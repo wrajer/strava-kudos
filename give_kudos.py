4731e06660e46ddb4f7b7b00f9c39c159ca78218
KrisBar <37506425+wrajer@users.noreply.github.com>
2024-05-21 20:48:20 +0200

    Update give_kudos.py

--- give_kudos.py
+++ give_kudos.py
@@ -14,7 +14,7 @@ class KudosGiver:
         self.EMAIL = os.environ.get('STRAVA_EMAIL')
         self.PASSWORD = os.environ.get('STRAVA_PASSWORD')
 
-        if self.EMAIL is None or self.PASSWORD is None:
+if self.EMAIL is None or self.PASSWORD is None:
             raise Exception(f"Must set environ variables EMAIL AND PASSWORD. \
                 e.g. run export STRAVA_EMAIL=YOUR_EMAIL")
 
@@ -68,7 +68,6 @@ class KudosGiver:
             self.own_profile_id = self.page.locator(".user-menu > a").get_attribute('href').split("/athletes/")[1]
             print("id", self.own_profile_id)
         except:
-            self.own_profile_id = '29740241'
             print("can't find own profile ID")
 
     def locate_kudos_buttons_and_maybe_give_kudos(self, web_feed_entry_locator) -> int:
@@ -166,32 +165,17 @@ class KudosGiver:
         ## Give Kudos on loaded page ##
         web_feed_entry_locator = self.page.locator(self.web_feed_entry_pattern)
         self.locate_kudos_buttons_and_maybe_give_kudos(web_feed_entry_locator=web_feed_entry_locator)
-    
-    def give_kudos_to_club(self, club_id):
-        """
-        Navigate to club page and give kudos to all activities
-        """
-        self.page.goto(os.path.join(BASE_URL, f"clubs/{club_id}/recent_activity?num_entries={self.num_entries}"))
-
-        ## Scrolling for lazy loading elements.
-        for _ in range(5):
-            self.page.keyboard.press('PageDown')
-            time.sleep(0.5)
-            self.page.keyboard.press('PageUp')
-
-        ## Give Kudos on loaded page ##
-        web_feed_entry_locator = self.page.locator(self.web_feed_entry_pattern)
-        self.locate_kudos_buttons_and_maybe_give_kudos(web_feed_entry_locator=web_feed_entry_locator)
-        
+        self.browser.close()
 
 
 def main():
     kg = KudosGiver()
     kg.email_login()
     kg.give_kudos()
-    kg.give_kudos_to_club(14342)
-    kg.browser.close()
 
 
 if __name__ == "__main__":
     main()
+       
+
+ 
